543b69ac7ed1aac0af5e2e64c19d768e938ecacf
Jinho <jangjinho81@gmail.com>
2025-06-01 07:56:30 +0000

    Add complete argument for N-TIDE training pipeline

--- arguments.py
+++ arguments.py
@@ -1,57 +1,60 @@
 import argparse
 
 def get_arguments():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='N-TIDE: Debiasing Unimodal Vision Models via Neutral Text Inversion with CLIP')
 
     # Seed config
-    parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--seed', type=int, default=42, help="Random seed for reproducibility")
 
     # Dataset config
-    parser.add_argument('--dataset_name', type=str, default='UTKFace')
-    parser.add_argument('--dataset_path', type=str, default='py97/UTKFace-Cropped')
-    parser.add_argument('--dataset_split_ratio', type=float, nargs=3, default=[0.8, 0.1, 0.1])
-    parser.add_argument('--gender_classes', type=str, nargs=2, default=['man', 'woman'])
-    parser.add_argument('--race_classes', type=str, nargs=5, default=['White', 'Black', 'Asian', 'Indian', 'Others'])
+    parser.add_argument('--dataset_name', type=str, default='UTKFace', help="Name of the dataset")
+    parser.add_argument('--dataset_path', type=str, default='py97/UTKFace-Cropped', help="Path to the dataset")
+    parser.add_argument('--dataset_split_ratio', type=float, nargs=3, default=[0.8, 0.1, 0.1], help="Train/Val/Test split ratio")
+    parser.add_argument('--gender_classes', type=str, nargs=2, default=['Man', 'Woman'], help="Gender class names")
+    parser.add_argument('--race_classes', type=str, nargs=5, default=['White', 'Black', 'Asian', 'Indian', 'Others'], help="Race class names")
 
     # Data Augmentation
-    parser.add_argument('--train_transform_type', type=str, default='strong', choices=['strong', 'weak'])
+    parser.add_argument('--train_transform_type', type=str, default='strong', choices=['strong', 'weak'], help="Training augmentation strategy")
 
     # Model config
-    parser.add_argument('--clip_text_prompt', type=str, default='A photo of a')
-    parser.add_argument('--clip_backbone', type=str, default='RN50')
-    parser.add_argument('--feature_dim', type=int, default=512)
+    parser.add_argument('--clip_text_prompt', type=str, default='', help="Text prompt for CLIP model (Default: Null-text)")
+    parser.add_argument('--clip_backbone', type=str, default='RN50', help="Backbone used in CLIP model's image encoder")
+    parser.add_argument('--feature_dim', type=int, default=512, help="Dimensionality of feature representation")
 
     # Train config
-    parser.add_argument('--train_mode', type=str, choices=['baseline', 'offline_teacher', 'offline_student'], default='baseline')
-    parser.add_argument('--batch_size', type=int, default=64)
-    parser.add_argument('--num_epochs', type=int, default=50)
-    parser.add_argument('--bf16', type=bool, default=False)
+    parser.add_argument('--train_mode', type=str, choices=['baseline', 'offline_teacher', 'offline_student'], default='baseline', help="Training mode type")
+    parser.add_argument('--batch_size', type=int, default=64, help="Batch size for training")
+    parser.add_argument('--num_epochs', type=int, default=50, help="Number of training epochs")
+    parser.add_argument('--bf16', action='store_true', help="Enable bfloat16 precision training")
 
     # -- CLIP model
-    parser.add_argument('--c_optimizer', type=str, default='AdamW')
-    parser.add_argument('--c_scheduler', type=str, default='Cosine')
-    parser.add_argument('--c_learning_rate', type=float, default=1e-4)
-    parser.add_argument('--c_weight_decay', type=float, default=1e-4)
-    parser.add_argument('--c_eta_min', type=float, default=1e-6)
+    parser.add_argument('--c_optimizer', type=str, default='AdamW', help="Optimizer for CLIP model")
+    parser.add_argument('--c_scheduler', type=str, default='Cosine', help="Scheduler for CLIP model")
+    parser.add_argument('--c_learning_rate', type=float, default=1e-4, help="Learning rate for CLIP model")
+    parser.add_argument('--c_weight_decay', type=float, default=1e-4, help="Weight decay for CLIP model")
+    parser.add_argument('--c_eta_min', type=float, default=1e-6, help="Minimum LR for CLIP scheduler")
 
     # -- CV model
-    parser.add_argument('--m_optimizer', type=str, default='SDG')
-    parser.add_argument('--m_scheduler', type=str, default='Cosine')
-    parser.add_argument('--m_learning_rate', type=float, default=1e-2)
-    parser.add_argument('--m_weight_decay', type=float, default=1e-4)
-    parser.add_argument('--m_eta_min', type=float, default=1e-5)
+    parser.add_argument('--m_optimizer', type=str, default='SGD', help="Optimizer for CV model")
+    parser.add_argument('--m_scheduler', type=str, default='Cosine', help="Scheduler for CV model")
+    parser.add_argument('--m_learning_rate', type=float, default=1e-2, help="Learning rate for CV model")
+    parser.add_argument('--m_weight_decay', type=float, default=1e-4, help="Weight decay for CV model")
+    parser.add_argument('--m_eta_min', type=float, default=1e-5, help="Minimum LR for CV scheduler")
 
-    parser.add_argument('--is_train', type=bool, default=True)
-    parser.add_argument('--use_wandb', type=bool, default=True)
-    parser.add_argument('--checkpoint_dir', type=str, default='./ckpt')
+    # -- Etc
+    parser.set_defaults(is_train=True)
+    parser.add_argument('--is_test', dest='is_train', action='store_false', help="Run in evaluation (test) mode")
+    parser.set_defaults(use_wandb=True)
+    parser.add_argument('--no_wandb', dest='use_wandb', action='store_false', help="Disable Wandb logging")
+    parser.add_argument('--checkpoint_dir', type=str, default='./ckpt', help="Directory to save checkpoints")
 
     # Loss Weights
-    parser.add_argument('--gender_smoothing', type=float, default=0.1)
-    parser.add_argument('--race_smoothing', type=float, default=0.1)
-    parser.add_argument('--b_lambda', type=float, default=0.5) # Baseline Loss
-    parser.add_argument('--c_lambda', type=int, default=0.5) # CLIP model Loss
-    parser.add_argument('--m_lambda', type=int, default=0.5) # CV model Loss
-    parser.add_argument('--alpha', type=int, default=0.5)
+    parser.add_argument('--gender_smoothing', type=float, default=0.1, help="Label smoothing factor for gender classification")
+    parser.add_argument('--race_smoothing', type=float, default=0.1, help="Label smoothing factor for race classification")
+    parser.add_argument('--c_lambda', type=float, default=0.5, help="Weight for Teacher (CLIP model) loss")
+    parser.add_argument('--m_lambda', type=float, default=0.5, help="Weight for Student (CV model) loss")
+    parser.add_argument('--alpha', type=float, default=0.5, help="Weight for Online KD loss")
+
 
     args = parser.parse_args()
     return args
--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import os
+import sys 
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -13,7 +14,7 @@ from src.dataset.get_dataset import get_dataset
 from src.model.get_model import get_model
 from src.trainer.BasicTrainer import BasicTrainer
 from src.trainer.OfflineTrainer import OfflineKDTrainer
-from src.trainer.OnlineTrainer import OnlineKDTrainer
+# from src.trainer.OnlineTrainer import OnlineKDTrainer
 
 def set_seed(seed):
     random.seed(seed)
@@ -28,103 +29,81 @@ def main(args):
     set_seed(args.seed)
 
     train_dataset, val_dataset, data_collator = get_dataset(args)
-
     num_workers = min(4, os.cpu_count()) 
+
     train_loader = torch.utils.data.DataLoader(
-        train_dataset, 
-        batch_size=args.batch_size, 
-        shuffle=True,  
-        collate_fn=data_collator,
-        drop_last=True,
-        pin_memory=True,
-        num_workers=num_workers,
-        persistent_workers=True
+        train_dataset, batch_size=args.batch_size, shuffle=True,
+        collate_fn=data_collator, drop_last=True, pin_memory=True,
+        num_workers=num_workers, persistent_workers=True
     )
     val_loader = torch.utils.data.DataLoader(
-        val_dataset, 
-        batch_size=args.batch_size, 
-        shuffle=False,  
-        collate_fn=data_collator,
-        pin_memory=True,
-        num_workers=num_workers,
-        persistent_workers=True
+        val_dataset, batch_size=args.batch_size, shuffle=False,
+        collate_fn=data_collator, pin_memory=True,
+        num_workers=num_workers, persistent_workers=True
     )
 
     clip, model = get_model(args, device)
     clip, model = clip.to(device), model.to(device)
-
     if args.bf16:
         clip, model = clip.to(torch.bfloat16), model.to(torch.bfloat16)
 
-    c_optimizer = torch.optim.AdamW(
-        clip.parameters(),
-        lr=args.c_learning_rate,
-        weight_decay=args.c_weight_decay
-    )
-    c_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-        c_optimizer,
-        T_max=args.num_epochs,
-        eta_min=args.c_eta_min
-    )
-
-    m_optimizer = torch.optim.SGD(
-        model.parameters(),
-        lr=args.m_learning_rate,
-        weight_decay=args.m_weight_decay,
-        momentum=0.9,
-        nesterov=True
-    )
-    m_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-        m_optimizer,
-        T_max=args.num_epochs,
-        eta_min=args.m_eta_min
-    )
-
+    # Basline Train: Fine-tuning ResNet50 (pretrained on ImageNet)
     if args.train_mode == 'baseline':
         if args.use_wandb:
             run_name = f"N-TIDE_Baseline_{time.strftime('%m%d_%H%M%S')}"
-            wandb.init(project='Intro-to-DL-N-TIDE', name=run_name, config=args) 
+            wandb.init(project='Intro-to-DL-N-TIDE', name=run_name, config=args)
 
-        trainer = BasicTrainer(
-            model=model,
-            train_loader=train_loader,
-            val_loader=val_loader,
-            optimizer=m_optimizer,
-            scheduler=m_scheduler,
-            device=device,
-            args=args,
+        optimizer = torch.optim.SGD(
+            model.parameters(), lr=args.m_learning_rate,
+            weight_decay=args.m_weight_decay, momentum=0.9, nesterov=True
         )
-
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optimizer, T_max=args.num_epochs, eta_min=args.m_eta_min
+        )
+        trainer = BasicTrainer(
+            model=model, 
+            train_loader=train_loader, val_loader=val_loader,
+            optimizer=optimizer, scheduler=scheduler, 
+            device=device, args=args
+        )
+    # Offline KD Train: Fine-tuning CLIP (Teacher model)
     elif args.train_mode == 'offline_teacher':
         if args.use_wandb:
             run_name = f"N-TIDE_Teacher_{time.strftime('%m%d_%H%M%S')}"
-            wandb.init(project='Intro-to-DL-N-TIDE', name=run_name, config=args) 
+            wandb.init(project='Intro-to-DL-N-TIDE', name=run_name, config=args)
 
+        optimizer = torch.optim.AdamW(
+            clip.parameters(), lr=args.c_learning_rate,
+            weight_decay=args.c_weight_decay
+        )
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optimizer, T_max=args.num_epochs, eta_min=args.c_eta_min
+        )
         trainer = OfflineKDTrainer(
-            model=clip,
-            model_type='teacher',
-            train_loader=train_loader,
-            val_loader=val_loader,
-            optimizer=c_optimizer,
-            scheduler=c_scheduler,
-            device=device,
-            args=args
-        )  
-        
+            model=clip, model_type='teacher',
+            train_loader=train_loader, val_loader=val_loader,
+            optimizer=optimizer, scheduler=scheduler,
+            device=device, args=args
+        )
+
+    # Offline KD Train: Fine-tuning ResNet50 (Student model)
     elif args.train_mode == 'offline_student':
         if args.use_wandb:
             run_name = f"N-TIDE_Student_{time.strftime('%m%d_%H%M%S')}"
-            wandb.init(project='Intro-to-DL-N-TIDE', name=run_name, config=args) 
+            wandb.init(project='Intro-to-DL-N-TIDE', name=run_name, config=args)
 
+        optimizer = torch.optim.SGD(
+            model.parameters(), lr=args.m_learning_rate,
+            weight_decay=args.m_weight_decay, momentum=0.9, nesterov=True
+        )
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optimizer, T_max=args.num_epochs, eta_min=args.m_eta_min
+        )
         trainer = OfflineKDTrainer(
-            model=model,
-            model_type='student',
-            train_loader=train_loader,
-            val_loader=val_loader,
-            optimizer=m_optimizer,
-            scheduler=m_scheduler,
-            device=device,
-            args=args
+            model=model, model_type='student',
+            train_loader=train_loader, val_loader=val_loader,
+            optimizer=optimizer, scheduler=scheduler,
+            device=device, args=args
         )
 
     trainer.train()
