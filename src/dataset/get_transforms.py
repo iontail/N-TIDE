fbfb69a6efd892aa4a8a1d6adcc1850e226afb55
Jinho <jangjinho81@gmail.com>
2025-06-02 09:28:43 +0000

    Fix: bug

--- src/dataset/get_transforms.py
+++ src/dataset/get_transforms.py
@@ -22,12 +22,18 @@ def get_transforms(args):
         train_transforms = transforms.Compose([
             transforms.RandomResizedCrop(224, scale=(0.7, 1.0)),
             transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomRotation(degrees=15),
+
+            # transforms.RandomRotation(degrees=15),
             transforms.ColorJitter(brightness=0.25, contrast=0.25, saturation=0.25, hue=0.05),
-            transforms.RandomApply([transforms.GaussianBlur(kernel_size=5)], p=0.5),
+            # transforms.RandomApply([transforms.GaussianBlur(kernel_size=5)], p=0.5),
+
+            transforms.RandomAffine(degrees=10, translate=(0.05, 0.05), scale=(0.9, 1.1), shear=5, fill=0),
+            transforms.RandomPerspective(distortion_scale=0.3, p=0.5),
+            transforms.RandomEqualize(p=0.3),
+
             transforms.ToTensor(),
             transforms.Normalize(mean=mean, std=std),
-            transforms.RandomErasing(p=0.5),
+            transforms.RandomErasing(p=0.3),
         ])
     elif args.train_transform_type == 'weak':
         train_transforms = transforms.Compose([
