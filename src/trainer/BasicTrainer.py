429e2c1e26f23571a9ab2cabc2e1027ef5d9c9b5
Jinho Jang <168627137+JangJinho65@users.noreply.github.com>
2025-06-06 15:31:34 +0900

    Update BasicTrainer.py

--- src/trainer/BasicTrainer.py
+++ src/trainer/BasicTrainer.py
@@ -161,11 +161,9 @@ class BasicTrainer:
         eval_log["eval_race_acc"] = race_correct / total
         
         for k, v in gender_race_results.items():
-            for metric, value in v.items():
-                eval_log[f"eval_gender_race/{k}/{metric}"] = value
+            eval_log[f"eval_gender_race/{k}"] = v
         for k, v in race_gender_results.items():
-            for metric, value in v.items():
-                eval_log[f"eval_race_gender/{k}/{metric}"] = value
+            eval_log[f"eval_race_gender/{k}/"] = v
         
         return eval_log
 
@@ -196,9 +194,11 @@ class BasicTrainer:
                     
                     'epoch/eval_loss': eval_log['eval_loss'],
                     'epoch/eval_gender_acc': eval_log['eval_gender_acc'],
-                    'epoch/eval_race_acc': eval_log['eval_race_acc']
-
+                    'epoch/eval_race_acc': eval_log['eval_race_acc'],
                     
+                    # Bias metrics
+                    **{f"epoch/{k}": v for k, v in eval_log.items() if k.startswith("eval_gender_race/")},
+                    **{f"epoch/{k}": v for k, v in eval_log.items() if k.startswith("eval_race_gender/")}
                 })
 
             if (epoch + 1) % 5 == 0 or (epoch + 1) == self.num_epochs:
