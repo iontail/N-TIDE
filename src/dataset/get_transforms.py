9bfbf2097b8306d06c02a8e3dbf5443f9d542d53
Jinho Jang <168627137+JangJinho65@users.noreply.github.com>
2025-06-11 06:27:46 +0900

    Update get_transforms.py

--- src/dataset/get_transforms.py
+++ src/dataset/get_transforms.py
@@ -2,12 +2,12 @@ import torchvision.transforms as transforms
 
 def get_transforms(args):
     # Data Augmentation
-    # For CV model pretrained on ImageNet
+    # For pretrained on ImageNet, Baseline 
     if args.experiment_type == 'baseline':
         mean = [0.485, 0.456, 0.406] 
         std = [0.229, 0.224, 0.225]
 
-    # For CLIP model (Open AI)
+    # For CLIP model (Open AI), Teacer model and Student model 
     elif args.experiment_type == 'offline_teacher' or args.experiment_type == 'offline_student': 
         mean = [0.48145466, 0.4578275, 0.40821073]
         std = [0.26862954, 0.26130258, 0.27577711]
