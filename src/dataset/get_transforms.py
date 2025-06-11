fe299367fb66fff1d23583dc84b23f3109685183
Jinho Jang <168627137+JangJinho65@users.noreply.github.com>
2025-06-11 21:51:00 +0900

    Update get_transforms.py

--- src/dataset/get_transforms.py
+++ src/dataset/get_transforms.py
@@ -7,7 +7,7 @@ def get_transforms(args):
         mean = [0.485, 0.456, 0.406] 
         std = [0.229, 0.224, 0.225]
 
-    # For CLIP model (Open AI), Teacer model and Student model 
+    # For KD Training, Teacer model and Student model 
     elif args.experiment_type == 'offline_teacher' or args.experiment_type == 'offline_student': 
         mean = [0.48145466, 0.4578275, 0.40821073]
         std = [0.26862954, 0.26130258, 0.27577711]
